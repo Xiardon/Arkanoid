88f893c9d364e92a92dd27d014ad8257df53c2d6
Xiardon <marcos.av@outlook.es>
2017-06-01 10:24:07 +0200

    Añadido contador de tiempo

---
+++ BolaClase.java
@@ -0,0 +1,145 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.*;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.animation.Animation;
+import java.util.Random;
+import javafx.scene.input.KeyCode;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+public class BolaClase extends Application
+{
+    private int velocidadEnX;
+    private int velocidadEnY;
+    private int velocidadPlataforma;
+    private static int RADIO = 20;
+    private int tiempoEnSegundos;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage escenario)
+    {
+        Group contenedor = new Group();
+
+        velocidadEnX = 1;
+        velocidadEnY = 1;
+        tiempoEnSegundos = 70;
+
+        Circle circulo = new Circle();
+        circulo.setFill(Color.RED);  
+        circulo.setRadius(RADIO);
+
+        Rectangle plataforma = new Rectangle();
+        plataforma.setWidth(50);
+        plataforma.setHeight(5);
+        plataforma.setTranslateX(225);
+        plataforma.setTranslateY(480);
+        plataforma.setFill(Color.BLUE);
+        contenedor.getChildren().add(plataforma);
+
+        velocidadPlataforma = 1;
+
+        Random aleatorio = new Random();
+
+        circulo.setCenterX(20 + aleatorio.nextInt(500 - 40));
+        circulo.setCenterY(50);
+        contenedor.getChildren().add(circulo);
+        
+        Label tiempoPasado = new Label("0");
+        contenedor.getChildren().add(tiempoPasado);
+
+        Scene escena = new Scene(contenedor, 500, 500);
+        escenario.setScene(escena);
+        escenario.show();
+
+        Timeline timeline = new Timeline();
+        KeyFrame keyframe = new KeyFrame(Duration.seconds(0.01), event -> {
+
+                        // Controlamos si la bola rebota a ziquierda o derecha
+                        if (circulo.getBoundsInParent().getMinX() <= 0 ||
+                        circulo.getBoundsInParent().getMaxX() >= escena.getWidth()) {
+                            velocidadEnX = -velocidadEnX;                              
+                        }
+
+                        // Conrolamos si la bola rebota arriba y abajo
+                        if (circulo.getBoundsInParent().getMinY() <= 0) {
+                            velocidadEnY = -velocidadEnY;
+                        }
+
+                        if (circulo.getBoundsInParent().getMaxY() == plataforma.getBoundsInParent().getMinY()) {
+                            double centroEnXDeLaBola = circulo.getBoundsInParent().getMinX() + RADIO;
+                            double minEnXDeLaPlataforma = plataforma.getBoundsInParent().getMinX();
+                            double maxEnXDeLaPlataforma = plataforma.getBoundsInParent().getMaxX();
+                            if ((centroEnXDeLaBola >= minEnXDeLaPlataforma) &&
+                            (centroEnXDeLaBola <= maxEnXDeLaPlataforma)) {
+                                //La bola esta sobre la plataforma
+                                velocidadEnY = -velocidadEnY;
+                            }
+                        }
+
+                        circulo.setTranslateX(circulo.getTranslateX() + velocidadEnX);
+                        circulo.setTranslateY(circulo.getTranslateY() + velocidadEnY);
+
+                        plataforma.setTranslateX(plataforma.getTranslateX() + velocidadPlataforma);
+                        if (plataforma.getBoundsInParent().getMinX() == 0  || 
+                        plataforma.getBoundsInParent().getMaxX() == escena.getWidth()) {
+                            velocidadPlataforma = 0;
+                        }
+                        
+                        // Actualizamos la etiqueta del tiempo
+                        int minutos = tiempoEnSegundos / 60;
+                        int segundos = tiempoEnSegundos % 60;
+                        tiempoPasado.setText(minutos + ":" + segundos);                        
+
+                        // Comrpobamos si el juego debe detenerse
+                        if (circulo.getBoundsInParent().getMinY() > escena.getHeight()) {
+                            Label mensajeGameOver = new Label("Game over");
+                            mensajeGameOver.setTranslateX(escena.getWidth() / 2);
+                            mensajeGameOver.setTranslateY(escena.getHeight() / 2);
+                            contenedor.getChildren().add(mensajeGameOver);
+                            timeline.stop();
+                        }
+                        
+                    });  
+        timeline.getKeyFrames().add(keyframe);
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();     
+
+        escena.setOnKeyPressed(event -> {
+                if (event.getCode() == KeyCode.RIGHT && 
+                plataforma.getBoundsInParent().getMaxX() != escena.getWidth()) {
+                    velocidadPlataforma = 1;
+                }
+                else if (event.getCode() == KeyCode.LEFT && 
+                plataforma.getBoundsInParent().getMinX() != 0) {
+                    velocidadPlataforma = -1;
+                }
+            });
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                tiempoEnSegundos++;
+            }                        
+        };
+        Timer timer = new Timer();
+        timer.schedule(tarea, 0, 1000);
+            
+    }
+
+}
--- ExperimentoBola.java
+++ ExperimentoBola.java
@@ -25,7 +25,7 @@ public class ExperimentoBola extends Application
     private double yMin;
     private Rectangle rectangulo = new Rectangle();
     private Timeline tm;
-    private Stage escenario;
+    private long tiempoEnSegundos;
 
     public static void main(String[] args){
         launch(args);
@@ -36,7 +36,7 @@ public class ExperimentoBola extends Application
     {
         startGame(escenario);
     }
-    
+
     public void startGame(Stage escenario){
         Group contenedor = new Group();
 
@@ -44,13 +44,13 @@ public class ExperimentoBola extends Application
         circulo.setRadius(20);
         circulo.setCenterX(new Random().nextDouble() + 20);
         circulo.setCenterY(250 - 20);
-        
+
         rectangulo.setFill(Color.BLUE);
         rectangulo.setX(250);
         rectangulo.setY(450);
         rectangulo.setHeight(10);
         rectangulo.setWidth(70);
-        
+
         Label finJuego = new Label("Game Over");
         finJuego.setVisible(false);
         finJuego.setMaxWidth(200);
@@ -59,64 +59,72 @@ public class ExperimentoBola extends Application
         finJuego.setAlignment(Pos.CENTER_LEFT);
         finJuego.layoutXProperty().bind(escenario.widthProperty().subtract(finJuego.widthProperty()).divide(2));
         finJuego.setTranslateY(200);
-        
-        tm = new Timeline(new KeyFrame(Duration.millis(10), event->{
-                        circulo.setTranslateX(circulo.getTranslateX() + velocidadCirculoX);
-                        circulo.setTranslateY(circulo.getTranslateY() + velocidadCirculoY);
-                        xMin = circulo.getBoundsInParent().getMinX();
-                        xMax = circulo.getBoundsInParent().getMaxX();
-                        yMin = circulo.getBoundsInParent().getMinY();
-                        yMax = circulo.getBoundsInParent().getMaxY();
-                        rectangulo.setTranslateX(rectangulo.getTranslateX() + velocidadRectangulo);
-                        if(xMax > 500 || xMin < 0){
-                            velocidadCirculoX = -velocidadCirculoX;
-                        }else if(yMin < 0){
-                            velocidadCirculoY = -velocidadCirculoY;
-                        }
-                        if(yMin > 500){
-                            finJuego.setVisible(true);
-                            tm.stop();
-                        }
-                        
-                        
-                        if(circulo.getBoundsInParent().intersects(rectangulo.getBoundsInParent())){
-                            velocidadCirculoY = -velocidadCirculoY;
-                        }
-                        if(rectangulo.getBoundsInParent().getMaxX() == 500 || rectangulo.getBoundsInParent().getMinX() == 0){
-                            velocidadRectangulo = 0;
-                        }
 
-                    }));
+        Label contador = new Label();
+        contador.setFont(new Font(20));
+        contador.setTranslateX(250);
+
+        TimerTask tarea = new TimerTask() {
+                @Override
+                public void run() {
+                    tiempoEnSegundos++;
+                }                        
+            };
+        Timer timer = new Timer();
+        timer.schedule(tarea, 0, 1000);
+
+        tm = new Timeline(new KeyFrame(Duration.millis(10), event->{
+                    long segundos = tiempoEnSegundos % 60;
+                    long minutos = tiempoEnSegundos / 60;
+                    contador.setText(minutos + ":" + segundos + "Tiempo");
+                    circulo.setTranslateX(circulo.getTranslateX() + velocidadCirculoX);
+                    circulo.setTranslateY(circulo.getTranslateY() + velocidadCirculoY);
+                    xMin = circulo.getBoundsInParent().getMinX();
+                    xMax = circulo.getBoundsInParent().getMaxX();
+                    yMin = circulo.getBoundsInParent().getMinY();
+                    yMax = circulo.getBoundsInParent().getMaxY();
+                    rectangulo.setTranslateX(rectangulo.getTranslateX() + velocidadRectangulo);
+                    if(xMax > 500 || xMin < 0){
+                        velocidadCirculoX = -velocidadCirculoX;
+                    }else if(yMin < 0){
+                        velocidadCirculoY = -velocidadCirculoY;
+                    }
+                    if(yMin > 500){
+                        finJuego.setVisible(true);
+                        tm.stop();
+                    }
+
+                    if(circulo.getBoundsInParent().intersects(rectangulo.getBoundsInParent())){
+                        velocidadCirculoY = -velocidadCirculoY;
+                    }
+                    if(rectangulo.getBoundsInParent().getMaxX() == 500 || rectangulo.getBoundsInParent().getMinX() == 0){
+                        velocidadRectangulo = 0;
+                    }
+
+                }));
         tm.setCycleCount(Timeline.INDEFINITE);
 
         tm.playFromStart();
 
         Button bt = new Button("Reset");
         bt.setOnAction(event -> {
-            tm.stop();
-            escenario.close();
-            startGame(escenario);
+                tm.stop();
+                escenario.close();
+                startGame(escenario);
             });
-            
-        contenedor.getChildren().addAll(circulo, rectangulo, bt, finJuego);
 
+        contenedor.getChildren().addAll(circulo, rectangulo, bt, finJuego, contador);
         Scene escena = new Scene(contenedor, 500, 500);
         escena.setOnKeyPressed(event -> {
-            if(event.getCode() == KeyCode.LEFT && !(rectangulo.getBoundsInParent().getMinX() == 0)){
-                velocidadRectangulo = -1;
-            }else if(event.getCode() == KeyCode.RIGHT && !(rectangulo.getBoundsInParent().getMaxX() == 500)){
-                velocidadRectangulo = 1;
-            }
-        });
+                if(event.getCode() == KeyCode.LEFT && !(rectangulo.getBoundsInParent().getMinX() == 0)){
+                    velocidadRectangulo = -1;
+                }else if(event.getCode() == KeyCode.RIGHT && !(rectangulo.getBoundsInParent().getMaxX() == 500)){
+                    velocidadRectangulo = 1;
+                }
+            });
         escenario.setScene(escena);
         escenario.show();
 
     }
-
-    
-    
-    
-    
-    
 
 }
